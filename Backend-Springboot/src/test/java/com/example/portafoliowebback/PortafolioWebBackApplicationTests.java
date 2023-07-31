22f890b5850f6278deb79a2514350582bc5b6635
Hilario Iñigo Menendez Picossi <hpicossi@gmail.com>
2023-07-31 18:51:51 -0300

    Inicio del proyecto del back

---
+++ Backend-Springboot/src/test/java/com/example/portafoliowebback/PortafolioWebBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.portafoliowebback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortafolioWebBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
