0da0c615d28211fc7abb1fa7f1d694de4e6c6287
Hilario Iñigo Menendez Picossi <hpicossi@gmail.com>
2024-04-26 23:36:55 -0300

    Modificación Swagger #1

--- BackEnd/src/main/java/com/example/portafolio/Entidad/Proyecto.java
+++ BackEnd/src/main/java/com/example/portafolio/Entidad/Proyecto.java
@@ -1,5 +1,7 @@
 package com.example.portafolio.Entidad;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
@@ -8,18 +10,23 @@ import lombok.Setter;
 @Getter
 @Setter
 @Table(name = "proyecto")
+@ApiModel(value = "Entidad Proyecto", description = "Representa un proyecto del usuario")
 public class Proyecto {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @ApiModelProperty(value = "ID único del proyecto", name = "idProyecto")
     private Long id;
 
+    @ApiModelProperty(value = "Nombre del proyecto", name = "nombre", required = true)
     private String nombre;
 
     @Lob
     @Column(nullable = true)
+    @ApiModelProperty(value = "Imagen del proyecto en formato JPEG", name = "imagen")
     private byte[] imagen;
 
+    @ApiModelProperty(value = "Descripción del proyecto", name = "descripcion")
     private String descripcion;
 
     public void setImagen(byte[] imagen) {
